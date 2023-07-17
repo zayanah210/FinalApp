3fbe3a4c11b0863c4cda03d729bbc0fdf7a43a98
zayanah <zayanahm1@gmail.com>
2023-07-17 09:53:06 -0500

    Initial Commit

---
+++ FinalApp/FinalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalAppApp.swift
+//  FinalApp
+//
+//  Created by scholar on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct FinalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
